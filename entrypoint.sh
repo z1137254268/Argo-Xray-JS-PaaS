40c0749ea9a421df5a7b9bdc645eab395a3bc339
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-01-25 23:30:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-WSPATH=glitch  # WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=de04add9-5c68-8bab-950c-08cd5320df18
-NEZHA_SERVER=server.nezha.org # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-NEZHA_PORT=5555
-NEZHA_KEY=olx2IwyG7BZjylaW3H
+WSPATH=neu  # WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+UUID=10974d1a-cbd6-4b6f-db1d-38d78b3fb109
+# NEZHA_SERVER=server.nezha.org # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
+# NEZHA_PORT=5555
+# NEZHA_KEY=olx2IwyG7BZjylaW3H
 
 # 安装系统依赖
 check_dependencies() {
